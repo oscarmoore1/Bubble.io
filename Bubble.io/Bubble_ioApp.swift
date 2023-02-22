1b4879a1eddb1ddb847094888b3c88e36fdbbada
oscarmoore1 <orhmoore@gmail.com>
2023-02-22 19:08:11 +1100

    Initial Commit

---
+++ Bubble.io/Bubble_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bubble_ioApp.swift
+//  Bubble.io
+//
+//  Created by Oscar Moore on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Bubble_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
